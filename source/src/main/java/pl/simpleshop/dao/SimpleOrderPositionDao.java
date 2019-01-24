d768da0d4904c13456e65c3dda432a6e7c59ead9
Infinity <miragae@gmail.com>
2019-01-24 19:24:07 +0100

    Changed Hibernate dialect to MariaDB Changed layout in order list dialog Fixed overflow issue Swapped orders and basket buttons Enabled sql logging

--- source/src/main/java/pl/simpleshop/dao/SimpleOrderPositionDao.java
+++ source/src/main/java/pl/simpleshop/dao/SimpleOrderPositionDao.java
@@ -3,6 +3,7 @@ package pl.simpleshop.dao;
 import javax.ejb.Stateless;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 
 import pl.simpleshop.model.*;
@@ -26,8 +27,10 @@ public class SimpleOrderPositionDao extends AbstractDao<SimpleOrderPosition> imp
         CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
         CriteriaQuery<SimpleOrderPosition> query = cb.createQuery(SimpleOrderPosition.class);
         Root<SimpleOrderPosition> from = query.from(SimpleOrderPosition.class);
+        Join<SimpleOrderPosition, Product> productJoin = from.join(SimpleOrderPosition_.product);
+        Join<Product, Category> categoryJoin = productJoin.join(Product_.category);
         query.where(cb.equal(from.get(SimpleOrderPosition_.order), order));
-        query.orderBy(cb.asc(from.get(SimpleOrderPosition_.id)));
+        query.orderBy(cb.asc(categoryJoin.get(Category_.name)));
         query.select(from);
         return getEntityManager().createQuery(query).getResultList();
     }
